643e355842ee5c94b40258777a9fa71b296c5698
Eldhose Babu <eldhosebabu197@gmail.com>
2024-10-19 19:46:19 -0400

    Added like functions

--- connect/pages/Home.js
+++ connect/pages/Home.js
@@ -4,11 +4,28 @@ import { supabase } from "../supabase";
 import ProfilePage from "./Profile";
 
 export default function Home({navigation, session, setSession}){
-    
+    const getLikeCount = async (postId) => {
+        const {data, error} = await supabase.from('post_likes').select('*', {count: 'exact'}).eq('post_id', postId);
+        console.log("Data", data.length)
+    }
+    const addLike = async (postId) => {
+        const {data, error} = await supabase.from('post_likes').insert([{'post_id': postId, 'userId': session.user.id}])
+        console.log("Error",error)
+        console.log("Data", data)
+    }
+    const unlike = async (postId) => {
+        const {data, error} = await supabase.from('post_likes').delete().eq('post_id', postId).eq('userId', session.user.id);
+        console.log("Data", data)
+        console.log("Error",  error);
+    }
     return(
     <View style={styles.container}>
         <Text style={{color: "#ff0000"}}>Home screen</Text>
+        <AppButton onPress={() => {getLikeCount("0b1fb65e-0e32-4068-92f5-d5527b18666e")}} title={"Get Like"}/>
+        <AppButton onPress={() => {addLike("0b1fb65e-0e32-4068-92f5-d5527b18666e")}} title={"Add Like"}/>
+        <AppButton onPress={() => {unlike("0b1fb65e-0e32-4068-92f5-d5527b18666e")}} title={"Remove Like"}/>
     </View>
+    
     
     );
 }
