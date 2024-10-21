54a40ffb297aa39de18419a3d21a1522e70c5fb5
Eldhose Babu <eldhosebabu197@gmail.com>
2024-10-20 22:14:58 -0400

    updated home.js

--- connect/pages/Home.js
+++ connect/pages/Home.js
@@ -6,16 +6,10 @@ import CommentOverlay from "../Components/CommentOverlay/CommentOverlay";
 import { useState } from "react";
 
 export default function Home({navigation, session, setSession}){
-    const [isCommentVisible, setIsCommentVisible] = useState(false);
     
     return(
     <View style={styles.container}>
         <Text style={{color: "#ff0000"}}>Home screen</Text>
-        <AppButton onPress={() => {getLikeCount("0b1fb65e-0e32-4068-92f5-d5527b18666e")}} title={"Get Like"}/>
-        <AppButton onPress={() => {addLike("0b1fb65e-0e32-4068-92f5-d5527b18666e")}} title={"Add Like"}/>
-        <AppButton onPress={() => {unlike("0b1fb65e-0e32-4068-92f5-d5527b18666e")}} title={"Remove Like"}/>
-        <AppButton onPress={() => {setIsCommentVisible(true)}} title={"Show Overlay"}/>
-            <CommentOverlay visible={isCommentVisible} onClose={()=>{setIsCommentVisible(false)}} postId={""}/>
     </View>
     );
 }
